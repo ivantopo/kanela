f8214714ccdd37f43e90265eacd64cd42cd92d95
Diego <diegolparra@gmail.com>
2018-01-26 23:03:01 -0300

    updated site

--- microsite/data/examples/Example.java
+++ microsite/data/examples/Example.java
@@ -40,7 +40,7 @@ public class PerformTaskMethodAdvisor {
 
 
 // tag:run:start
-public class TimeSpentJava {
+public class TimeSpent {
     public static void main(String... args) {
         for(int i = 0; i < 10; i++) {
             new Worker().performTask();
